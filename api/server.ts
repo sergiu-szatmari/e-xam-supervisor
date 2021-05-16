d494880ea44916e6efd1e8c0983f1c0ab9a41977
Sergiu <szatmari.sergiu@gmail.com>
2021-05-16 23:44:17 +0300

    Min

--- api/server.ts
+++ api/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import http from 'http';
 import config from 'config';
 import { ExpressPeerServer } from 'peer';
@@ -20,6 +20,8 @@ peerServer.on('connection', (client) => {
 });
 
 app.use(peerServer);
+
+app.get('/', (req: Request, res: Response) => res.status(200).json({ message: 'E-xam Supervisor Server works' }));
 
 server.listen(port, () => {
   console.log(`Server is ${ green('up and running') } on port ${ yellow(port)}`);
